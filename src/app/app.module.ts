a26c6abba798e846fef7245a2a19b78cd7a3f094
Gustavo Benedito <gustavo.benedito@ws-solution.com>
2021-12-30 00:19:58 -0300

    desenvolvimento da pagina inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadercomponentComponent } from './components/headercomponent/headercomponent.component';
+import { MaincomponentComponent } from './components/maincomponent/maincomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadercomponentComponent,
+    MaincomponentComponent
   ],
   imports: [
     BrowserModule,
